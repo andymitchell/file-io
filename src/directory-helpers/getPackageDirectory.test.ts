afd1dcae0940f72815efb12dc9272f5e811c41d5
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2024-06-03 15:30:33 +0100

    getPackageDirectory should not find this package - instead it should find the one below

--- src/directory-helpers/getPackageDirectory.test.ts
+++ src/directory-helpers/getPackageDirectory.test.ts
@@ -1,9 +1,25 @@
-import { getPackageDirectory } from "./getPackageDirectory"
+
+import { getPackageDirectory, getPackageDirectoryInternal } from "./getPackageDirectory"
 
 describe('getPackageDirectory', () => {
-    test('basic', async () => {
+    test('skips us because we are is_andyrmitchell_file_io_package', async () => {
         const dir = await getPackageDirectory();
+        expect(dir).toBe('');
+        
+    }, 1000*60)
+
+    test('finds us if forced', async () => {
+        const dir = await getPackageDirectoryInternal(undefined, undefined, undefined, {'testing_skip_package_check': true});
         const parts = dir.split('/');
         expect(parts[parts.length-1]).toBe('file-io');
+        
+    }, 1000*60)
+
+    test('skip fake-this-pkg because it is is_andyrmitchell_file_io_package', async () => {
+        const root = await getPackageDirectoryInternal(undefined, undefined, undefined, {'testing_skip_package_check': true});
+        const dir = await getPackageDirectory(`${root}/test-env/pkg-root/node_modules/fake-this-pkg`);
+        const parts = dir.split('/');
+        expect(parts[parts.length-1]).toBe('pkg-root');
+        
     }, 1000*60)
 })
--- src/directory-helpers/getPackageDirectory.ts
+++ src/directory-helpers/getPackageDirectory.ts
@@ -6,29 +6,68 @@ import { getInvokedScriptDirectory } from './getInvokedScriptDirectory';
 
 
 /**
- * Find the nearest ancestor with a package.json, including when it's consumed within node modules. 
- * Use it to find relative paths from the root that are consistent in deployment.
+ * Find the nearest ancestor with package.json. Returns an absolute path. 
  * 
- * E.g. if in ./node_modules/pg-queue, it'll return "<path to>/node_modules/pg-queue"
+ * The default startFromDirectory:
+ *  - If your code is packaged and deployed in a consumer's node_modules: your package's root (i.e. node_modules/your-package)
+ *  - If you're running your code directly (including a test environment), it'll be the root
+ * 
+ * @param startFromDirectory 
+ * @param fileIo 
  * @returns 
  */
-export async function getPackageDirectory(fileIo?:IFileIo):Promise<string> {
+export async function getPackageDirectory(startFromDirectory?: string, fileIo?:IFileIo):Promise<string> {
+    return getPackageDirectoryInternal(startFromDirectory, fileIo);
+}
+
+export async function getPackageDirectoryInternal(startFromDirectory?: string, fileIo?:IFileIo, recursing?:boolean, options?: {testing_skip_package_check?: boolean}):Promise<string> {
     if( !fileIo ) fileIo = fileIoNode;
-
-    const startFrom = await getInvokedScriptDirectory();
-
-    let currentDirectory = startFrom;
+    if( !startFromDirectory ) {
+        startFromDirectory = await getInvokedScriptDirectory();
+    }
+    
+    let currentDirectory = startFromDirectory;
+    let foundPackageJsonUri:string | undefined;
     while(true) {
         const files = await fileIo.list_files(currentDirectory, {file_pattern: /^package\.json$/i});
         const file = files[0];
         if( file ) {
-            return file.path;
+            foundPackageJsonUri = file.uri;
+            break;
         }
         currentDirectory =  fileIo.directory_name(currentDirectory);
         if( currentDirectory==='/' || currentDirectory.length<=2 ) {
-            return '';
+            break;
         }
     }
+
+    // Check against returning this package - we want the package.json below our packaged self
+    if( foundPackageJsonUri ) {
+        const packageJson = await fileIo.read(foundPackageJsonUri);
+        let foundThisPackage = false;
+        try {
+            const pkg = JSON.parse(packageJson!);
+            foundThisPackage = pkg.is_andyrmitchell_file_io_package;
+                
+
+        } catch(e) {
+            debugger;
+        }
+
+        if( foundThisPackage && !options?.testing_skip_package_check ) {
+            if( recursing ) {
+                throw new Error("Should not recurse twice");
+            }
+            // Got to go to the next level
+            const parentDirectory = fileIo.directory_name(fileIo.directory_name(foundPackageJsonUri));
+            return getPackageDirectoryInternal(parentDirectory, fileIo, true);
+        } else {
+            return fileIo.directory_name(foundPackageJsonUri);
+        }
+    }
+    return '';
+
+    
     
 
 }
