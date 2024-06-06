0fcc8197a708518a6740f2b9c0fe56960842ad48
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2024-06-06 12:00:15 +0100

    Add test helper

--- src/user-input/index.ts
+++ src/user-input/index.ts
@@ -1,15 +1,18 @@
 import { UserInputNode } from "./UserInputNode";
 import { getDirectoryFromUser } from "./common/getDirectoryFromUser";
+import { TestQuestionAnswerMap, TestUserInput } from "./test-helper/TestUserInput";
 import { Answer, IUserInput, QuestionChain, QuestionChoice } from "./types";
 
 export {
     UserInputNode,
-    getDirectoryFromUser
+    getDirectoryFromUser,
+    TestUserInput
 }
 
 export type {
     IUserInput,
     QuestionChain,
     QuestionChoice,
-    Answer
+    Answer,
+    TestQuestionAnswerMap
 }
